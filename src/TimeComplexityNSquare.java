4a2fefe656537df820b60cff8e5cd320d8cde42c
Vedica Vriti <vedica1ga19cs179@gmail.com>
2025-06-03 23:22:37 +0530

    "hello world program pushed"

---
+++ src/TimeComplexityNSquare.java
@@ -0,0 +1,15 @@
+public class TimeComplexityNSquare {
+    public static void main(String[] args) {
+       printNumbers(10);
+    }
+
+    public static void printNumbers(int n){
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.println(i+" "+j);
+            }
+        }
+    }
+}

86495321dfdad5d2c960c1fe54f5a4313464eff3
Vedica Vriti <vedica1ga19cs179@gmail.com>
2025-06-03 23:43:54 +0530

    "modified"

--- src/TimeComplexityNSquare.java
+++ src/TimeComplexityNSquare.java
@@ -2,7 +2,7 @@ public class TimeComplexityNSquare {
     public static void main(String[] args) {
        printNumbers(10);
     }
-
+//Time Complexity n square
     public static void printNumbers(int n){
         for(int i=0;i<n;i++)
         {

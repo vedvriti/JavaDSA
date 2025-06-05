6a4693219bb65d8a49fb1ef30cb92f276e6b6f01
Vedica Vriti <vedica1ga19cs179@gmail.com>
2025-06-05 23:34:51 +0530

    "Program 7"

---
+++ src/DistinctElements.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class DistinctElements {
+    public static void main(String[] args){
+        int[] arr = {12, 10, 9, 45, 2, 10, 10, 45};
+        ArrayList<Integer> res = distinctElementsInAnArray(arr);
+        for(int i: res) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static ArrayList<Integer> distinctElementsInAnArray(int[] arr)
+    {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            int num = arr[i];
+            if(!set.contains(num)) {
+              System.out.print(num + " ");
+              set.add(num);
+            }
+        }
+        return new ArrayList<>(set);
+    }
+}

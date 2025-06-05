ee08ece2d6448277bedcec8b0386c5467b48b8eb
Vedica Vriti <vedica1ga19cs179@gmail.com>
2025-06-05 22:26:37 +0530

    "Program 5"

---
+++ src/Cookie.java
@@ -0,0 +1,13 @@
+public class Cookie {
+    private String color;
+    public Cookie(String color){
+        this.color = color;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+
+}
